8083af75f681823e5238589ea91d677ba4aa4cb8
Norm Vilmer <norm.vilmer@wnco.com>
2018-12-05 13:03:39 -0600

    Fixes for 100% unittest coverage

--- daugherty/utils.py
+++ daugherty/utils.py
@@ -1,14 +1,19 @@
 __author__ = 'Norm Vilmer'
 __project__ = 'LunchAndLearnDec2018'
 
+
 class Singleton(type):
-    __instances = {}
+    _instances = {}
+
     def __call__(cls, *args, **kwargs):
-        if cls not in cls.__instances:
-            cls.__instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls.__instances[cls]
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+
+        return cls._instances[cls]
+
 
 class Utils(metaclass=Singleton):
-    def evenNumbers(self, numbersIn):
-        return list(filter(lambda x: x % 2 == 0, numbersIn))
+    @staticmethod
+    def even(numbers_in):
+        return list(filter(lambda x: x % 2 == 0, numbers_in))
 
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,13 +1,14 @@
-from unittest import TestCase
+import unittest
 from daugherty.utils import Utils
 
-class UtilsTest(TestCase):
+
+class UtilsTest(unittest.TestCase):
     def testUtilsIsSingleton(self):
-        a = Utils
-        b = Utils()
-        self.assertEqual(a, b)
+        first_instance = Utils()
+        second_instance = Utils()
+        self.assertIs(first_instance, second_instance)
 
     def testEvenNumbers(self):
-        numbersIn = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        numbersOut = Utils.evenNumbers(self, numbersIn)
-        self.assertTrue(all(num % 2 == 0 for num in numbersOut))
+        numbers_in = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        numbers_out = Utils.even(numbers_in)
+        self.assertTrue(all(num % 2 == 0 for num in numbers_out))
