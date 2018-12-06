4927b9c67473c8c8679514d47c6a25fe7231a9fb
norm.vilmer@daugherty.com <norm.vilmer@daugherty.com>
2018-12-05 22:48:02 -0600

    Fixed PEP8 errors; added more example code

--- daugherty/utils.py
+++ daugherty/utils.py
@@ -2,18 +2,41 @@ __author__ = 'Norm Vilmer'
 __project__ = 'LunchAndLearnDec2018'
 
 
+# Single as a metaclass
 class Singleton(type):
-    _instances = {}
+    __instances = {}
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
 
-        return cls._instances[cls]
+        return cls.__instances[cls]
 
 
 class Utils(metaclass=Singleton):
     @staticmethod
-    def even(numbers_in):
+    def filter_even_numbers(numbers_in):
         return list(filter(lambda x: x % 2 == 0, numbers_in))
+
+    @staticmethod
+    def filter_odd_numbers(numbers_in):
+        return list(filter(lambda x: x % 2 != 0, numbers_in))
+
+class Config(metaclass=Singleton):
+    __props = {}
+
+    def load_properties(self, filepath):
+        with open(filepath, "rt") as propfile:
+            for line in propfile:
+                string_line = line.strip()
+                if string_line and not string_line.startswith('#'):
+                    parts = string_line.split('=')
+                    key = parts[0].strip()
+                    value = '='.join(parts[1:]).strip().strip('"')
+                    self.__props[key] = value
+        return self.__props
+
+    def getProperties(self):
+        return self.__props
+
 
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,14 +1,37 @@
-import unittest
-from daugherty.utils import Utils
+import unittest, tempfile, shutil
+from os import path
+from daugherty.utils import Utils, Config
 
 
 class UtilsTest(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
     def testUtilsIsSingleton(self):
         first_instance = Utils()
         second_instance = Utils()
         self.assertIs(first_instance, second_instance)
 
-    def testEvenNumbers(self):
+    def testFilterEvenNumbers(self):
         numbers_in = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        numbers_out = Utils.even(numbers_in)
+        numbers_out = Utils.filter_even_numbers(numbers_in)
         self.assertTrue(all(num % 2 == 0 for num in numbers_out))
+
+    def testFilterOddNumbers(self):
+        numbers_in = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        numbers_out = Utils.filter_odd_numbers(numbers_in)
+        self.assertTrue(all(num % 2 != 0 for num in numbers_out))
+
+    def testLoadConfig(self):
+        with open(path.join(self.test_dir, 'config.properties'), 'w') as config_file:
+            config_file.write('# Test config.properties\n')
+            config_file.write('user.name=bob\n')
+            config_file.write('user.location=dallas\n')
+
+        config = Config()
+        config.load_properties(self.test_dir + '/config.properties')
+        self.assertEqual(config.getProperties()['user.name'], 'bob')
+        self.assertEqual(config.getProperties()['user.location'], 'dallas')
